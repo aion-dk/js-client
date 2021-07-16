19f911cf766caf0cc68a540d3430c54618e9b56d
hulub <stefan.patachi@gmail.com>
2021-07-16 14:32:52 +0200

    EncryptVotes generates the proof of correct encryption

--- lib/av_client/encrypt_votes.ts
+++ lib/av_client/encrypt_votes.ts
@@ -3,24 +3,38 @@ const Crypto = require('./aion_crypto.js')()
 export default class EncryptVotes {
   encrypt(contests: ContestIndexed<EncryptionData>, encryptionKey: PublicKey) {
 
-      const cryptograms: ContestIndexed<Cryptogram> = {}
+      const response: ContestIndexed<EncryptionResponse> = {}
       for (let contestId in contests) {
-          cryptograms[contestId] = Crypto.encryptVote(
+          const { cryptogram, randomness } = Crypto.encryptVote(
               contests[contestId].voteEncodingType,
               contests[contestId].vote,
               contests[contestId].emptyCryptogram,
               encryptionKey
-          )
+          );
+          const proof = Crypto.generateDiscreteLogarithmProof(randomness)
+
+          response[contestId] = {
+              cryptogram: cryptogram,
+              randomness: randomness,
+              proof: proof
+          }
       }
 
-      return cryptograms;
+      return response;
   }
 }
 
 type PublicKey = string;
 type Cryptogram = string;
+type Proof = string;
+type BigNum = string;
 interface ContestIndexed<Type> {
     [index: string]: Type;
+}
+type EncryptionResponse = {
+    cryptogram: Cryptogram;
+    randomness: BigNum;
+    proof: Proof;
 }
 type EncryptionData = {
     vote: string;
