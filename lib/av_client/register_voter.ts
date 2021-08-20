63bc35219a7696789424d3c9ae943dfd71a61196
Adomas Sliuzinskas <adomas@aion.dk>
2021-08-20 15:46:51 +0200

    Ballots now come from registration, overwrite election config ballot data

--- lib/av_client/register_voter.ts
+++ lib/av_client/register_voter.ts
@@ -23,6 +23,7 @@ export default class RegisterVoter {
         this.client.bulletinBoard.setVoterSessionUuid(data.voterSessionUuid);
         this.client.voterIdentifier = data.voterIdentifier;
         this.client.emptyCryptograms = data.emptyCryptograms;
+        this.client.electionConfig.ballots = data.ballots;
       }
     )
     randomKeyPair(); // TODO: remove, this just increases deterministic randomness offset for tests
