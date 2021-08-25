91cb0bb63f4bd256cf55703b929e088b75552141
Adomas Sliuzinskas <adomas@aion.dk>
2021-08-25 09:52:27 +0200

    Walkthrough test embedded in documentation
    Using custom `typedoc-wide-theme` to make the layout wider,
    to fit in code examples with long strings.
    
    Why write non-executable documentation, when you can
    embed executable tests in it!

---
+++ test/readme_example.test.ts
@@ -0,0 +1,32 @@
+import { AVClient } from '../lib/av_client';
+import { expect } from 'chai';
+import { readmeTestSetup, readmeTestTeardown } from './readme_example_helper';
+
+describe('entire voter flow using OTP authorization', () => {
+  beforeEach(() => readmeTestSetup());
+  afterEach(() => readmeTestTeardown());
+
+  it('returns a receipt', async () => {
+    const client = new AVClient('http://localhost:3000/test/app');
+
+    const requestAccessCodeResult = await client.requestAccessCode('some PII info');
+    expect(requestAccessCodeResult).to.eq('OK')
+
+    const validateAccessCodeResult = await client.validateAccessCode('1234', 'voter@foo.bar');
+    expect(validateAccessCodeResult).to.eq('OK');
+
+    const cvr = { '1': 'option1', '2': 'optiona' };
+    const fingerprint = await client.constructBallotCryptograms(cvr);
+    expect(fingerprint).to.eq('da46ec752fd9197c0d77e6d843924b082b8b23350e8ac5fd454051dc1bf85ad2');
+
+    const affidavit = 'some bytes, most likely as binary PDF';
+    const receipt = await client.submitBallotCryptograms(affidavit);
+    expect(receipt).to.eql({
+      previousBoardHash: 'd8d9742271592d1b212bbd4cbbbe357aef8e00cdbdf312df95e9cf9a1a921465',
+      boardHash: '87abbdea83326ba124a99f8f56ba4748f9df97022a869c297aad94c460804c03',
+      registeredAt: '2020-03-01T10:00:00.000+01:00',
+      serverSignature: 'bfaffbaf8778abce29ea98ebc90ca91e091881480e18ef31da815d181cead1f6,8977ad08d4fc3b1d9be311d93cf8e98178142685c5fbbf703abf2188a8d1c862',
+      voteSubmissionId: 6
+    });
+  });
+});
