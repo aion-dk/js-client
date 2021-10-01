375968025f21379a1a2ce6c32d62ca8e2cd100a8
Adomas Sliuzinskas <adomas@aion.dk>
2021-10-01 11:22:45 +0200

    Self-explanatory code for valid options including selected option

--- lib/av_client/cvr_validation.ts
+++ lib/av_client/cvr_validation.ts
@@ -7,8 +7,12 @@ const validateCvr = (cvr: CastVoteRecord, contests: Ballot[]) : ValidationResult
   const hasVotedForAllContests = JSON.stringify(Object.keys(cvr).map(k => parseInt(k))) === JSON.stringify(contests.map(c => c.id))
 
   const areSelectedOptionsValid = Object.keys(cvr).every(function(contestId) {
-    const contest = contests.find(b => b.id.toString() == contestId)
-    return contest && contest.options.some(o => o.handle == cvr[contestId])
+    const contest = contests.find(_contest => _contest.id.toString() == contestId)
+    if (!contest) return false;
+
+    const validOptions = contest.options.map(option => option.handle);
+    const selectedOption = cvr[contestId];
+    return validOptions.includes(selectedOption);
   })
 
   if(!hasVotedForAllContests) return ':invalid_contest'
