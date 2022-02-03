d37a63b467ce10601f12f51e6f54f38b9992dfe1
Mads Christiansen <madc@aion.dk>
2022-02-03 10:30:21 +0100

    use mocks by default

--- test/walkthrough.test.ts
+++ test/walkthrough.test.ts
@@ -10,7 +10,7 @@ import {
 } from './test_helpers';
 import { recordResponses } from './test_helpers'
 
-const USE_MOCK = false;
+const USE_MOCK = true;
 
 describe('entire voter flow using OTP authorization', () => {
   let sandbox;
@@ -43,8 +43,6 @@ describe('entire voter flow using OTP authorization', () => {
   });
 
   it('returns a receipt', async () => {
-
-
     // TODO: DEPRECATED?
     // expectedNetworkRequests.push(nock(bulletinBoardHost).post('/mobile-api/us/challenge_empty_cryptograms')
     //   .replyWithFile(200, __dirname + '/replies/otp_flow/post_us_app_challenge_empty_cryptograms.json'));
