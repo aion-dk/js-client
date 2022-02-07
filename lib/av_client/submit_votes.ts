cd2b27052c7bff04ce9f02266ff58a330646b9ad
Mads Christiansen <madc@aion.dk>
2022-02-07 15:29:42 +0100

    Cleanup deprecated code

--- lib/av_client/submit_votes.ts
+++ lib/av_client/submit_votes.ts
@@ -1,27 +1,11 @@
-import { ContestMap, OpenableEnvelope, SealedEnvelope, Affidavit, BallotBoxReceipt } from './types'
-import { AcknowledgedBoardHash, signVotes, sealEnvelopes, assertValidReceipt, encryptAES, fingerprint } from './sign'
+import {  Affidavit } from './types'
+import { encryptAES } from './sign'
 import { BulletinBoard } from './connectors/bulletin_board'
-
-type SignAndSubmitArguments = {
-  voterIdentifier: string;
-  encryptedVotes: ContestMap<OpenableEnvelope>;
-  voterPrivateKey: string;
-  electionSigningPublicKey: string,
-  encryptedAffidavit: string
-}
-
-type SubmitArgs = {
-  contentHash: string, 
-  voterSignature: string,
-  cryptogramsWithProofs: ContestMap<SealedEnvelope>
-  encryptedAffidavit: string
-}
 
 type AffidavitConfig = {
   curve: string;
   encryptionKey: string;
 }
-
 export default class SubmitVotes {
   bulletinBoard: BulletinBoard;
 
