89dde2a1353925d58df5f12d533e187ed63e137d
Mads Christiansen <madc@aion.dk>
2022-02-09 09:19:34 +0100

    Finalize each cryptogram per contest rather than first only

--- lib/av_client/actions/finalize_ballot_cryptograms.ts
+++ lib/av_client/actions/finalize_ballot_cryptograms.ts
@@ -1,15 +1,13 @@
 import EncryptVotes from '../encrypt_votes';
 import { ContestMap, OpenableEnvelope } from '../types';
 
-export const finalizeBallotCryptograms = (clientCryptograms: ContestMap<OpenableEnvelope>, serverCryptograms: ContestMap<string[]>): ContestMap<string> => { 
-  const finalizedCryptograms = Object.keys(clientCryptograms).map((contestUuid, i) => {
-    const finalizedCryptogram = EncryptVotes.homomorphicallyAddCryptograms(
-      // TODO: we operate with the assumption that only one cryptogram is used per contest
-      clientCryptograms[contestUuid].cryptograms[0],
-      serverCryptograms[contestUuid][0]
-    );
-
-    return [contestUuid, [finalizedCryptogram]]
-  })
-  return Object.fromEntries(finalizedCryptograms)
+export const finalizeBallotCryptograms = (clientCryptograms: ContestMap<OpenableEnvelope>, serverCryptograms: ContestMap<string[]>): ContestMap<string[]> => { 
+  const finalizedCryptograms = Object.fromEntries(Object.entries(clientCryptograms)
+      .map(([contestUuid, crypto]) => [contestUuid, crypto.cryptograms.map((cryptogram, i) => EncryptVotes.homomorphicallyAddCryptograms(
+        cryptogram,
+        serverCryptograms[contestUuid][i] // TODO: Can we assume the cryptograms are in the same order?
+      ))])
+  );
+  
+  return finalizedCryptograms
 }
