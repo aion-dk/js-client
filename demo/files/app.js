8c6b367989365aad2d2bc765719c81997105de82
Mads Christiansen <madc@aion.dk>
2022-03-03 20:00:05 +0100

    update verifier with decryption

--- demo/files/app.js
+++ demo/files/app.js
@@ -1,6 +1,7 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 const verifier = new AssemblyVoting.AVVerifier("http://us-avx:3000/dbb/us/api");
+verifier.initialize()
 
 $(document).ready(() => {
   $("form#findBallot").submit((event) => {
@@ -9,22 +10,24 @@ $(document).ready(() => {
 
     verifier.findBallot(address).then((response) => {
       const verifyKeyView = $("#submitVerifierKey");
-      console.log('ballot', response)
       verifyKeyView.find("#verification-code").text(response.address);
       
       verifyKeyView.collapse("show");
 
       verifier.pollForSpoilRequest().then((spoilRequestAddress) => {
-        console.log('spoil address', spoilRequestAddress)
 
         verifier.submitVerifierKey(spoilRequestAddress).then(verifierItem => {
-          console.log('verifier', verifierItem)
           $("form#submitVerifierKey #verification-code").text(verifierItem.address)
 
           verifier.pollForCommitmentOpening().then(commitmentOpenings => {
-            console.log('commitment openings', commitmentOpenings)
+            const selections = verifier.decryptBallot()
+            var el = $("#choices")
+            el.html("")
+            Object.keys(selections).forEach(key => {
+                el.append(`<li>Contest: ${key} - Choice: ${selections[key]}</li>`)
+            })
+
             $('#showChoices').collapse("show");
-            $("#choices").text(JSON.stringify(commitmentOpenings))
           })
         })
       });
