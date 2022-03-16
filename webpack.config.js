bc7410719b09d8b628c299bb291478fd1bcbb171
Mads Christiansen <madc@aion.dk>
2022-03-16 13:38:36 +0100

    remove problematic jsonwebtoken package

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 module.exports = {
   entry: './dist/lib/av_verifier.js',
   resolve: {
-    fallback: {"crypto": false}
+    fallback: {
+      "crypto": false
+    }
   },
   optimization: {
     minimize: false,
