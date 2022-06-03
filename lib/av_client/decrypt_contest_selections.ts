fe27623b4ae4674e94395f1edd7cfa5cef355588
Michael Andersen <michael@assemblyvoting.com>
2022-06-03 11:34:30 +0200

    Adding encryption and decryption functions for contest selections

---
+++ lib/av_client/decrypt_contest_selections.ts
@@ -0,0 +1,53 @@
+import { ContestConfigMap, ContestSelection, ContestMap, CommitmentOpening } from "./types"
+import { ElGamalPointCryptogram, addBigNums } from "./aion_crypto"
+import { bignumFromHex, pointFromHex } from "./crypto/util"
+import { pointsToBytes } from "./encoding/point_encoding"
+import { byteArrayToContestSelection } from "./encoding/byte_encoding"
+
+export function decryptContestSelections(
+  contestConfigs: ContestConfigMap,
+  encryptionKey: string,
+  cryptograms: ContestMap<string[]>,
+  boardCommitmentOpening: CommitmentOpening,
+  voterCommitmentOpening: CommitmentOpening
+): ContestSelection[] {
+
+  return Object.keys(cryptograms).map(function(contestReference) {
+    const contestConfig = contestConfigs[contestReference]
+    const contestCryptograms = cryptograms[contestReference]
+    const randomizers = combineRandomizers(contestReference, boardCommitmentOpening, voterCommitmentOpening)
+
+    const points = decryptPoints(contestCryptograms, randomizers, encryptionKey)
+    const maxSize = contestConfig.markingType.encoding.maxSize
+    const encodedContestSelection = pointsToBytes(points, maxSize)
+    return byteArrayToContestSelection(contestConfig, encodedContestSelection)
+  })
+}
+
+function combineRandomizers( 
+  contestReference: string, 
+  boardCommitmentOpening: CommitmentOpening, 
+  voterCommitmentOpening: CommitmentOpening 
+){
+  const br = boardCommitmentOpening.randomizers[contestReference]
+  const vr = voterCommitmentOpening.randomizers[contestReference]
+
+  return Object.keys(vr).map(i => addBigNums(vr[i], br[i]))
+}
+
+function decryptPoints( cryptograms: string[], randomizers: string[], encryptionKey: string ){
+  return cryptograms.map((cryptogram, index) => {
+    const randomizer = randomizers[index]
+    return decryptPoint(cryptogram, randomizer, encryptionKey)
+  })
+}
+
+function decryptPoint( cryptogram: string, randomizer: string, encryptionKey: string ){
+  const elGamalCryptogram = ElGamalPointCryptogram.fromString(cryptogram)
+  const publicKey = pointFromHex(encryptionKey).toEccPoint()
+  const randomizerBn = bignumFromHex(randomizer).toBn()
+
+  // invert cryptogram so you can decrypt using the randomness
+  const newCryptogram = new ElGamalPointCryptogram(publicKey, elGamalCryptogram.ciphertext_point)
+  return newCryptogram.decrypt(randomizerBn)
+}
---
+++ lib/av_client/encrypt_contest_selections.ts
@@ -0,0 +1,44 @@
+import { ContestConfigMap, ContestEnvelope, ContestConfig, ContestSelection } from "./types";
+import { randomBN, ElGamalPointCryptogram } from "./aion_crypto";
+import { bignumToHex, pointFromHex } from "./crypto/util";
+import { bytesToPoints } from "./encoding/point_encoding";
+import { contestSelectionToByteArray } from "./encoding/byte_encoding";
+
+export function encryptContestSelections(
+  contestConfigs: ContestConfigMap,
+  contestSelections: ContestSelection[],
+  encryptionKey: string
+): ContestEnvelope[] {
+  return contestSelections.map(contestSelection => {
+    const contestConfig = contestConfigs[contestSelection.reference]
+    return encryptContestSelection(contestConfig, contestSelection, encryptionKey)
+  })
+}
+
+function encryptContestSelection(
+  contestConfig: ContestConfig, 
+  contestSelection: ContestSelection, 
+  encryptionKey: string
+): ContestEnvelope {
+  const encodedContestSelection = contestSelectionToByteArray(contestConfig, contestSelection)
+  const contestPoints = bytesToPoints(encodedContestSelection)
+
+  // Encrypt the contestPoints
+  const encryptionKeyPoint = pointFromHex(encryptionKey).toEccPoint();
+
+  const contestEnvelope: ContestEnvelope = {
+    reference: contestSelection.reference,
+    cryptograms: [],
+    randomizers: []
+  }
+
+  contestPoints.map(votePoint => {
+    const randomizerBN = randomBN();
+    const cryptogram = ElGamalPointCryptogram.encrypt(votePoint, encryptionKeyPoint, randomizerBN);
+
+    contestEnvelope.randomizers.push(bignumToHex(randomizerBN))
+    contestEnvelope.cryptograms.push(cryptogram.toString())
+  })
+
+  return contestEnvelope
+}
