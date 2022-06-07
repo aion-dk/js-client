b51f99301da192c24da699463dbf81d3bbb03d0e
Mads Christiansen <madc@aion.dk>
2022-06-07 10:37:05 +0200

    trying again

--- test/walkthrough.test.ts
+++ test/walkthrough.test.ts
@@ -1,7 +1,6 @@
 import { AVClient } from '../lib/av_client';
 import { expect } from 'chai';
 import axios from 'axios';
-import { BallotBoxReceipt } from '../lib/av_client';
 import nock = require('nock');
 import {
   resetDeterminism,
