b61fe13f006557254aa392a3fd67ff2fa2bc776d
Mads Christiansen <madc@aion.dk>
2022-06-07 10:30:35 +0200

    BalotBoxReceipt changes

--- test/walkthrough.test.ts
+++ test/walkthrough.test.ts
@@ -1,6 +1,7 @@
 import { AVClient } from '../lib/av_client';
 import { expect } from 'chai';
 import axios from 'axios';
+import { BallotBoxReceipt } from '../lib/av_client';
 import nock = require('nock');
 import {
   resetDeterminism,
@@ -92,7 +93,7 @@ describe('entire voter flow using OTP authorization', () => {
 
       const affidavit = Buffer.from('some bytes, most likely as binary PDF').toString('base64');
       const receipt = await client.castBallot(affidavit);
-      expect(typeof receipt === "string").to.be.true;
+      expect(receipt.trackingCode.length).to.eql(7)
 
       if(USE_MOCK)
         expectedNetworkRequests.forEach((mock) => mock.done());
