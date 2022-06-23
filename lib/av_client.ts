05687b1eb108a23ff4198a2d7f006c6d819d0887
Michael Andersen <michael@assemblyvoting.com>
2022-06-23 16:13:41 +0200

    Fix imports in avClient

--- lib/av_client.ts
+++ lib/av_client.ts
@@ -1,7 +1,7 @@
 import { BulletinBoard } from './av_client/connectors/bulletin_board';
 import VoterAuthorizationCoordinator from './av_client/connectors/voter_authorization_coordinator';
 import { OTPProvider, IdentityConfirmationToken } from "./av_client/connectors/otp_provider";
-import { extractContestSelections } from './util/nist_json_converter';
+import { extractContestSelections } from './util/nist_cvr_extractor';
 import { AVVerifier } from './av_verifier';
 import { constructContestEnvelopes } from './av_client/construct_contest_envelopes';
 import { KeyPair, Affidavit, VerifierItem, CommitmentOpening, SpoilRequestItem, ElectionConfig, BallotSelection, ContestEnvelope, BallotConfig, BallotStatus } from './av_client/types';
