ad79e9521538ee075039ecebb59d4446e1b84b4c
Alexis Toledo <alexis.toledo@.local>
2022-12-01 12:32:01 +0100

    we

--- test/verifier/readable_contest_selections.test.ts
+++ test/verifier/readable_contest_selections.test.ts
@@ -6,10 +6,184 @@ import { LatestConfig } from '../../lib/av_client';
 
 describe('getReadbleContestSelections', () => {
   let verifier: AVVerifier;
+  const latestConfig: LatestConfig = {
+    items: {
+      thresholdConfig: {
+        content: {
+          encryptionKey: "03d8c46ae42cd7f95009ddf444eebc5b18e2cad34aad94129ffc54b5606b5638f4"
+        }
+      },
+      voterAuthorizerConfig: {
+        content: {
+          identityProvider: {
+            contextUuid: "adb99ef7-7992-4bc9-a54b-239d64d97f91",
+            publicKey: "030edb8bebffcd417b3bbb33a2ea95508b710b46dddd08de1a6d333768264b4a51",
+            url: "http://otp:3001"
+          },
+          voterAuthorizer: {
+            contextUuid: "fce38c77-3418-41b5-8dc5-915557fe7aa5",
+            publicKey: "03020e0548dac980b65218f60134ee8d15ef16a9631e217d95240ec06c277bfc8b",
+            url: "http://voter-authorizer:3002/",
+            authorizationMode: "proof-of-identity"
+          }
+        },
+      },
+      ballotConfigs: {
+        precinct_4_bedrock: {
+          content: {
+            reference: "precinct_4_bedrock",
+            voterGroup: "precinct_4_bedrock",
+            contestReferences: [
+              "contest ref 1",
+              "contest ref 2"
+            ]
+          },
+        },
+      },
+      contestConfigs: {
+        "contest ref 1": {
+          content: {
+            reference: "contest ref 1",
+            title: {
+              en: "First ballot"
+            },
+            subtitle: {
+              en: "First ballot"
+            },
+            description: {
+              en: "First ballot"
+            },
+            markingType: {
+              blankSubmission: "disabled",
+              minMarks: 1,
+              maxMarks: 1,
+              encoding: {
+                codeSize: 1,
+                maxSize: 1,
+                cryptogramCount: 1
+              }
+            },
+            resultType: {
+              name: "resultType name not matter right now"
+            },
+            options: [
+              {
+                reference: "option ref 1",
+                code: 1,
+                title: {
+                  en: "Option 1"
+                },
+                subtitle: {
+                  en: "Option 1"
+                },
+                description: {
+                  en: "Option 1"
+                },
+              },
+              {
+                reference: "option ref 2",
+                code: 2,
+                title: {
+                  en: "Option 2"
+                },
+                subtitle: {
+                  en: "Option 2"
+                },
+                description: {
+                  en: "Option 2"
+                },
+              }
+            ]
+          }
+        },
+        "contest ref 2": {
+          content: {
+            reference: "contest ref 2",
+            title: {
+              en: "Second ballot"
+            },
+            subtitle: {
+              en: "Second ballot"
+            },
+            description: {
+              en: "Second ballot"
+            },
+            markingType: {
+              blankSubmission: "disabled",
+              minMarks: 1,
+              maxMarks: 1,
+              encoding: {
+                codeSize: 1,
+                maxSize: 1,
+                cryptogramCount: 1
+              }
+            },
+            resultType: {
+              name: "resultType name not matter right now"
+            },
+            options: [
+              {
+                reference: "option ref 1",
+                code: 1,
+                title: {
+                  en: "Option 1"
+                },
+                subtitle: {
+                  en: "Option 1"
+                },
+                description: {
+                  en: "Option 1"
+                },
+              },
+              {
+                reference: "option ref 2",
+                code: 2,
+                title: {
+                  en: "Option 2"
+                },
+                subtitle: {
+                  en: "Option 2"
+                },
+                description: {
+                  en: "Option 2"
+                },
+              }
+            ]
+          }
+        },
+      },
+      electionConfig: {
+        content: {
+          title: {
+            en: "Some US Election"
+          },
+          uuid: "bc1b1ed0-943e-4647-bfc1-0633ba08c05e",
+        }
+      },
+      genesisConfig: {
+        content: {
+          ballotAcceptance: "inferred",
+          eaCurveName: "secp256k1",
+          eaPublicKey: "03d247cd0170769326a0dcd321653bf5986618fcdda418da0eb45184f8f350ed61",
+          electionSlug: "us",
+          publicKey: "037b9c074d3756dc2b8f0eda4aa9d36d661e79d0e2b50da38b5d56a0154b630838",
+          resultExtraction: "throughout-election"
+        }
+      },
+      latestConfigItem: {
+        address: "0e4cc630c0fd8f6ca3f4a2f4329ab440969044a7dd470038f6c5ae17c5f6ed23",
+        author: "Election Admin App",
+        parentAddress: "dfd82ccd645048358fe92cb48988ee4487fdd390fb657bf68733fae9bf308cfd",
+        previousAddress: "dfd82ccd645048358fe92cb48988ee4487fdd390fb657bf68733fae9bf308cfd",
+        registeredAt: "2022-11-30T13:57:38.451Z",
+        signature: "ccb3e5af13908cdbff5b41b0e5a349bddb573adc017fa4f0ba436c45c3b7f7f8,18f361cc6944ffae8ab0b9f34e8b1c49a5bb78fb0a09889e1ab405d183b11e9b",
+      }
+    },
+  };
 
   beforeEach(async () => {
     verifier = new AVVerifier(bulletinBoardHost + 'us');
-    await verifier.initialize()
+    await verifier.initialize(latestConfig)
   });
 
   context('given valid ballot', () => {
@@ -20,7 +194,7 @@ describe('getReadbleContestSelections', () => {
       },
       {
         reference: 'contest ref 2',
-        optionSelections: [{ reference: 'option ref 3' }]
+        optionSelections: [{ reference: 'option ref 2' }]
       },
     ]
 
@@ -42,8 +216,8 @@ describe('getReadbleContestSelections', () => {
           title: 'Second ballot',
           optionSelections: [
             { 
-              reference: 'option ref 3',
-              title: 'Option 3'
+              reference: 'option ref 2',
+              title: 'Option 2'
             }
           ]
         },
