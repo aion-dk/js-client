5efa85cc1a9692cdb8b7eed188af6ea09729d43a
Alexis Toledo <alexis.toledo@.local>
2022-12-01 12:17:25 +0100

    idk

--- test/election_configuration_validation.test.ts
+++ test/election_configuration_validation.test.ts
@@ -5,7 +5,7 @@ import { InvalidConfigError } from '../lib/av_client/errors';
 
 describe('election configuration validation', () => {
   let client: AVClient;
-  const config: LatestConfig = {
+  const latestConfig: LatestConfig = {
     items: {
       thresholdConfig: {
         content: {
@@ -187,24 +187,24 @@ describe('election configuration validation', () => {
   context('OTP provider URL is empty', () => {
     it('fails with an error', async () => {
       
-      config.items.voterAuthorizerConfig.content.identityProvider.url = '';
+      latestConfig.items.voterAuthorizerConfig.content.identityProvider.url = '';
       
       await expectError(
-        client.initialize(config),
+        client.initialize(latestConfig),
         InvalidConfigError,
         'Received invalid election configuration. Errors: Configuration is missing OTP Provider URL'
         );
 
-      config.items.voterAuthorizerConfig.content.identityProvider.url = 'http://otp:3001';
+        latestConfig.items.voterAuthorizerConfig.content.identityProvider.url = 'http://otp:3001';
     });
   });
 
   context('Voter Authorizer URL is empty', () => {
     it('fails with an error', async () => {
-      config.items.voterAuthorizerConfig.content.voterAuthorizer.url = '';
+      latestConfig.items.voterAuthorizerConfig.content.voterAuthorizer.url = '';
 
       await expectError(
-        client.initialize(config),
+        client.initialize(latestConfig),
         InvalidConfigError,
         'Received invalid election configuration. Errors: Configuration is missing Voter Authorizer URL'
       );
@@ -213,10 +213,10 @@ describe('election configuration validation', () => {
 
   context('services key is missing', () => {
     it('fails with an error', async () => {
-      delete (config as any).items.voterAuthorizerConfig;
+      delete (latestConfig as any).items.voterAuthorizerConfig;
 
       await expectError(
-        client.initialize(config),
+        client.initialize(latestConfig),
         InvalidConfigError,
         "Received invalid election configuration. Errors: Configuration is missing OTP Provider URL,\n" +
         "Configuration is missing OTP Provider election context uuid,\n" +
