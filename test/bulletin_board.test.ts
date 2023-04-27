3d7acfe3f700ebe30632dd38b5daa67fa87a5b85
Martin Laursen <martin@assemblyvoting.com>
2023-04-27 14:28:22 +0200

    a test i guess

--- test/bulletin_board.test.ts
+++ test/bulletin_board.test.ts
@@ -10,18 +10,19 @@ import {
   NetworkError,
   UnsupportedServerReplyError
 } from '../lib/av_client/errors';
+import {expect} from "chai";
+
+let bulletinBoard: BulletinBoard;
+
+beforeEach(() => {
+  bulletinBoard = new BulletinBoard(bulletinBoardHost + 'us');
+});
+
+afterEach(() => {
+  nock.cleanAll();
+});
 
 describe('BulletinBoard#registerVoter', () => {
-  let bulletinBoard: BulletinBoard;
-
-  beforeEach(() => {
-    bulletinBoard = new BulletinBoard(bulletinBoardHost + 'us');
-  });
-
-  afterEach(() => {
-    nock.cleanAll();
-  });
-
   context('public key already registered on Bulletin Board', () => {
     it('returns an error', async () => {
       nock(bulletinBoardHost).post('/us/voting/registrations')
@@ -55,6 +56,68 @@ describe('BulletinBoard#registerVoter', () => {
 
       await expectError(
         bulletinBoard.createVoterRegistration('authToken', 'parent_address'),
+        NetworkError,
+        'Network error. Could not connect to Bulletin Board.'
+      );
+    });
+  });
+});
+
+
+describe('BulletinBoard#expireVoterSessions', () => {
+
+  context('nothing goes wrong', ()=>{
+    it('returns a response', async () => {
+      nock(bulletinBoardHost).post('/us/voting/expirations')
+        .reply(200, { expiredSessions: 0 });
+
+      expect(
+        await bulletinBoard.expireVoterSessions('authToken', 'parent_address')
+      ).to.be.a("object");
+    });
+
+    it('the response data indicates how many sessions were expired', async () => {
+      nock(bulletinBoardHost).post('/us/voting/expirations')
+        .reply(200, { expiredSessions: 0 });
+
+      const response = await bulletinBoard.expireVoterSessions('authToken', 'parent_address');
+      expect(response.data).to.eql({expiredSessions: 0});
+    });
+  })
+
+  context('public key already registered on Bulletin Board', () => {
+    it('returns an error', async () => {
+      nock(bulletinBoardHost).post('/us/voting/expirations')
+        .reply(403, { error: { code: 13, description: 'Public key error' }});
+
+      await expectError(
+        bulletinBoard.expireVoterSessions('authToken', 'parent_address'),
+        BulletinBoardError,
+        'Public key error'
+      );
+    });
+  });
+
+  context('Bulletin Board returns unknown error', () => {
+    it('returns an error', async () => {
+      nock(bulletinBoardHost).post('/us/voting/expirations')
+        .reply(500, { foo: 'bar' });
+
+      await expectError(
+        bulletinBoard.expireVoterSessions('authToken', 'parent_address'),
+        UnsupportedServerReplyError,
+        'Unsupported Bulletin Board server error message: {"foo":"bar"}'
+      );
+    });
+  });
+
+  context('Bulletin Board becomes unreachable', () => {
+    it('returns an error', async () => {
+      nock(bulletinBoardHost).post('/us/voting/expirations')
+        .replyWithError('Some network error');
+
+      await expectError(
+        bulletinBoard.expireVoterSessions('authToken', 'parent_address'),
         NetworkError,
         'Network error. Could not connect to Bulletin Board.'
       );
