622d832110f3cfae97898c3a39671e479be1bc22
Ștefan Patachi <stefan@assemblyvoting.com>
2023-06-12 14:51:36 +0200

    encoder implementation

--- lib/av_crypto/encoder.ts
+++ lib/av_crypto/encoder.ts
@@ -1,18 +1,21 @@
-import {BigNumber, SjclEllipticalCurve, SjclEllipticalPoint, SjclHashStatic} from "./sjcl";
-import * as sjcl from "sjcl-with-all";
+import {SjclEllipticalPoint} from "./sjcl";
 import {Curve} from "./curve";
-import {hexToPoint} from "./utils";
+import {hexToPoint, pointToHex} from "./utils";
 
 const ADJUSTING_BYTE_COUNT = 1
 const ENCODING_ITERATIONS = ADJUSTING_BYTE_COUNT * 256
 
 export class Encoder {
   private curve: Curve
-  private pointEncodingByeSize: number
+  public pointEncodingByteSize: number
 
   constructor(curve: Curve) {
     this.curve = curve
-    this.pointEncodingByeSize = Math.floor(curve.degree() / 8.0) - ADJUSTING_BYTE_COUNT
+    this.pointEncodingByteSize = Math.floor(curve.degree() / 8.0) - ADJUSTING_BYTE_COUNT
+  }
+
+  public pointsToBytes(points: Array<SjclEllipticalPoint>): Array<number> {
+    return points.flatMap( point => this.pointToBytes(point))
   }
 
   public bytesToPoints(bytes: Array<number>): Array<SjclEllipticalPoint> {
@@ -22,8 +25,8 @@ export class Encoder {
 
     const points: Array<SjclEllipticalPoint> = []
 
-    for (let i = 0; i < bytes.length; i += this.pointEncodingByeSize) {
-      const bytesSlice = bytes.slice(i, i + this.pointEncodingByeSize);
+    for (let i = 0; i < bytes.length; i += this.pointEncodingByteSize) {
+      const bytesSlice = bytes.slice(i, i + this.pointEncodingByteSize);
       const paddedBytesSlice = this.padBytes(bytesSlice);
       points.push(this.encodeIntoPoint(paddedBytesSlice))
     }
@@ -32,14 +35,13 @@ export class Encoder {
   }
 
   private padBytes(bytes: Array<number>): Array<number> {
-    const paddingBytes = Array(this.pointEncodingByeSize - bytes.length).fill(0)
+    const paddingBytes = Array(this.pointEncodingByteSize - bytes.length).fill(0)
 
     return bytes.concat(paddingBytes)
   }
 
   private encodeIntoPoint(bytes: Array<number>): SjclEllipticalPoint {
-    // FIXME: b.toString(16) produces "1" instead of "01"
-    const bytesHex = bytes.map(b => b.toString(16)).join('')
+    const bytesHex = bytes.map(b => this.byteToHex(b)).join('')
 
     for (let i = 0; i < ENCODING_ITERATIONS ; i++) {
       try {
@@ -51,12 +53,34 @@ export class Encoder {
   }
 
   private generatePoint(bytesHex: string, i: number): SjclEllipticalPoint {
-    // FIXME: i.toString(16) produces "1" instead of "01"
-    const adjustmentHex = i.toString(16)
+    const adjustmentHex = this.byteToHex(i)
     // pad `00` to the left because of the secp521r1 curve
     const paddingHex = Array(this.curve.scalarHexSize() - adjustmentHex.length - bytesHex.length).fill("0").join('')
     const pointHex = "02" + paddingHex + adjustmentHex + bytesHex
 
     return hexToPoint(pointHex, this.curve)
+  }
+
+  private pointToBytes(point: SjclEllipticalPoint): Array<number> {
+    if (point.isIdentity) {
+      throw new Error("unable to decode infinity point")
+    }
+
+    const pointHex = pointToHex(point)
+    const bytesHex = pointHex.slice(-this.pointEncodignHexSize())
+    const bytes : Array<number> = [];
+    for (let i = 0; i < bytesHex.length; i += 2) {
+      bytes.push(parseInt(bytesHex.substring(i, i + 2), 16));
+    }
+
+    return bytes;
+  }
+
+  private byteToHex(byte: number): string {
+    return byte.toString(16).padStart(2, '0')
+  }
+
+  private pointEncodignHexSize(): number {
+    return this.pointEncodingByteSize * 2
   }
 }
--- test/av_crypto/encoder.test.ts
+++ test/av_crypto/encoder.test.ts
@@ -1,12 +1,45 @@
 import { expect } from "chai";
 import {Curve} from "../../lib/av_crypto/curve";
-import {pointToHex} from "../../lib/av_crypto/utils";
-import {hexString} from "./test_helpers";
+import {hexToPoint, infinityPoint, pointToHex} from "../../lib/av_crypto/utils";
+import {fixedPoint1, fixedPoint2, hexString} from "./test_helpers";
 import {Encoder} from "../../lib/av_crypto/encoder";
 
 describe("Encoder", () => {
   const curve = new Curve("k256")
   const encoder = new Encoder(curve)
+
+  describe("constructor", () => {
+    it ("constructs an encoder", () => {
+      expect(encoder.pointEncodingByteSize).to.equal(31)
+    })
+
+    context("with curve secp256r1", () => {
+      const curve = new Curve("c256")
+      const encoder = new Encoder(curve)
+
+      it ("constructs an encoder", () => {
+        expect(encoder.pointEncodingByteSize).to.equal(31)
+      })
+    })
+
+    context("with curve secp384r1", () => {
+      const curve = new Curve("c384")
+      const encoder = new Encoder(curve)
+
+      it ("constructs an encoder", () => {
+        expect(encoder.pointEncodingByteSize).to.equal(47)
+      })
+    })
+
+    context("with curve secp521r1", () => {
+      const curve = new Curve("c521")
+      const encoder = new Encoder(curve)
+
+      it ("constructs an encoder", () => {
+        expect(encoder.pointEncodingByteSize).to.equal(64)
+      })
+    })
+  })
 
   describe("bytesToPoints()", () => {
     const bytes = [1, 2, 3]
@@ -22,9 +55,65 @@ describe("Encoder", () => {
       ))
     })
 
+    context("with max amount of bytes for one point", () => {
+      const bytes = Array.from(Array(31).keys())
 
+      it("produces one point", () => {
+        const points = encoder.bytesToPoints(bytes)
 
+        expect(points.length).to.be.equal(1);
+      })
+    })
 
+    context("with bytes that fit in two points", () => {
+      const bytes = Array.from(Array(32).keys())
+
+      it("produces two points", () => {
+        const points = encoder.bytesToPoints(bytes)
+
+        expect(points.length).to.be.equal(2);
+      })
+    })
+
+    context("when bytes have max value", () => {
+      const bytes = Array(31).fill(255);
+
+      it("produces one point", () => {
+        const points = encoder.bytesToPoints(bytes)
+
+        expect(points.length).to.be.equal(1);
+      })
+    })
+
+    context("with byte too high", () => {
+      const bytes = [256]
+
+      it("throws error", () => {
+        expect(() => {
+          encoder.bytesToPoints(bytes)
+        }).to.throw("input must be an array of bytes (between 0 and 255)")
+      })
+    })
+
+    context("with negative byte", () => {
+      const bytes = [-10]
+
+      it("throws error", () => {
+        expect(() => {
+          encoder.bytesToPoints(bytes)
+        }).to.throw("input must be an array of bytes (between 0 and 255)")
+      })
+    })
+
+    context("with empty byte array", () => {
+      const bytes = []
+
+      it("produces no points", () => {
+        const points = encoder.bytesToPoints(bytes)
+
+        expect(points.length).to.be.equal(0);
+      })
+    })
 
     context("with curve secp521r1", () => {
       const curve = new Curve("c521")
@@ -41,6 +130,65 @@ describe("Encoder", () => {
           "00000000 00000000 00000000 00000000" +
           "00000000 00000000 00000000 00000000"
         ))
+      })
+    })
+  })
+
+  describe("pointsToBytes()", () => {
+    const points = [fixedPoint1(curve)]
+
+    it("decodes an array of bytes", () => {
+      const bytes = encoder.pointsToBytes(points)
+
+      expect(bytes.length).to.be.equal(31);
+    })
+
+    context("with a specific point", () => {
+      const hex = hexString(
+        "02" +
+        "01010203 00000000 00000000 00000000" +
+        "00000000 00000000 00000000 00000000"
+      )
+      const point = hexToPoint(hex, curve)
+      const points = [point]
+
+      it("decodes the right bytes", () => {
+        const bytes = encoder.pointsToBytes(points)
+        const expectedBytes = [1, 2, 3].concat(Array(28).fill(0))
+
+        expect(bytes).to.be.eql(expectedBytes);
+      })
+    })
+
+    context("with multiple points", () => {
+      const points = [fixedPoint1(curve), fixedPoint2(curve)]
+
+      it("decodes an array of bytes", () => {
+        const bytes = encoder.pointsToBytes(points)
+
+        expect(bytes.length).to.be.equal(62);
+      })
+    })
+
+    context("with the infinity point", () => {
+      const points = [infinityPoint(curve)]
+
+      it("throws error", () => {
+        expect(() => {
+          encoder.pointsToBytes(points)
+        }).to.throw("unable to decode infinity point")
+      })
+    })
+
+    context("with curve secp521r1", () => {
+      const curve = new Curve("c521")
+      const encoder = new Encoder(curve)
+      const points = [fixedPoint1(curve)]
+
+      it("decodes an array of bytes", () => {
+        const bytes = encoder.pointsToBytes(points)
+
+        expect(bytes.length).to.be.equal(64);
       })
     })
   })
