9fc197b6b5e7f46689392b665551ad5db5f9c1cf
Mads Christiansen <madc@assemblyvoting.com>
2023-11-17 14:06:00 +0100

    more alphas

--- lib/validators/belgiumBallotValidator.ts
+++ lib/validators/belgiumBallotValidator.ts
@@ -47,13 +47,22 @@ class BelgiumBallotValidator {
    */
   private partyExclusive(choices: OptionSelection[]) {
     const selectedReferences = this.selectedReferences(choices)
+
+    // Cannot vote on two parties at once
     let error = this.contest.options.filter(option => selectedReferences.includes(option.reference)).length > 1
 
     let found = false
     this.contest.options.forEach(parent =>  {
-      if (!parent.children) return
+      // Cannot vote for a party and a child in another party
+      if (selectedReferences.includes(parent.reference)) {
+        const remainingReferences = selectedReferences.filter(reference => reference !== parent.reference)
+        const childReferences = parent.children?.map(child => child.reference)
 
-      const childChosen = parent.children.some(child => selectedReferences.includes(child.reference))
+        error = remainingReferences.some(reference => !childReferences?.includes(reference))
+      }
+
+      // Cannot vote for children in two different parties
+      const childChosen = parent.children?.some(child => selectedReferences.includes(child.reference))
       if(found && childChosen) {
         error = true
       } else if(childChosen) {
