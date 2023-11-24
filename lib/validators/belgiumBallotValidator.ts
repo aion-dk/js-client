38befddc4dcd44ab2b93d609bb9989c45bf5c9e5
Alexander Ibsen <alexander@assemblyvoting.com>
2023-11-24 16:34:00 +0100

    Remove validation that is not needed

--- lib/validators/belgiumBallotValidator.ts
+++ lib/validators/belgiumBallotValidator.ts
@@ -10,17 +10,12 @@ class BelgiumBallotValidator {
     const errors: string[] = [];
 
     if (this.partyExclusive(selectionPile.optionSelections)) errors.push('cross_party_voting');
-    if (this.parentExclusive(selectionPile.optionSelections)) errors.push('parent_and_child_selected');
 
     return errors
   }
 
   private selectedReferences(optionSelections: OptionSelection[]) {
     return optionSelections.map((os) => os.reference);
-  }
-
-  isComplete(selectionPile: SelectionPile) {
-    return this.validate(selectionPile).length == 0;
   }
 
   /**
@@ -49,26 +44,6 @@ class BelgiumBallotValidator {
       } else if(childChosen) {
         found = true
       }
-    })
-
-    return error
-  }
-
-  /**
-   * Not allowed to vote on a parent and child
-   */
-  private parentExclusive(choices: OptionSelection[]) {
-    const selectedReferences = this.selectedReferences(choices)
-    let error = false
-
-    const selectedParents = this.contest.options.filter(option => selectedReferences.includes(option.reference))
-    if (!selectedParents.length) return
-
-    selectedParents.forEach(parent =>  {
-
-      if (!parent.children) return
-
-      if (parent.children.find(child => selectedReferences.includes(child.reference))) error = true
     })
 
     return error
