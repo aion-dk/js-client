3011b11df5632550e3c4e48cf747873a2acb6184
av-alexistoledo <112619860+av-alexistoledo@users.noreply.github.com>
2023-11-28 13:37:46 +0100

    Null check selection piles (#278)
    * null check
    
    * cleaning
    
    ---------
    
    Co-authored-by: Mads Christiansen <madc@assemblyvoting.com>

--- lib/av_client/validate_selections.ts
+++ lib/av_client/validate_selections.ts
@@ -55,14 +55,17 @@ function validateSelectionPile(pile: SelectionPile, markingType: MarkingType, op
     throw new CorruptSelectionError('Blank submissions are not allowed in this contest')
   }
 
-  // Validate that mark count is within bounds
-  const calculatedMinMarks = !isBlank && getOption(pile.optionSelections[0]).exclusive ? 1 : markingType.minMarks;
-  if( !isBlank && !withinBounds(calculatedMinMarks, pile.optionSelections.length, markingType.maxMarks) ){
-    throw new CorruptSelectionError('Contest selection does not contain a valid amount of option selections')
-  }
-
   pile.optionSelections.forEach(optionSelection => {
     const option = getOption(optionSelection)
+
+    // Validate that mark count is within bounds
+
+    const isExlusive = pile.optionSelections?.length && option?.exclusive
+    const calculatedMinMarks = !isBlank && isExlusive ? 1 : markingType.minMarks;
+
+    if( !isBlank && !withinBounds(calculatedMinMarks, pile.optionSelections.length, markingType.maxMarks) ){
+      throw new CorruptSelectionError('Contest selection does not contain a valid amount of option selections')
+    }
 
     if( option.writeIn ){
       if( !optionSelection.text ){
