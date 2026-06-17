baba55a86ba2b344d1a85738e687beee91429258
Mads Christiansen <madc@assemblyvoting.com>
2026-06-17 11:13:31 +0200

    slim down tests

--- test/set_registration_channel.test.ts
+++ test/set_registration_channel.test.ts
@@ -27,53 +27,5 @@ describe('setRegistrationChannel', () => {
     const registrationChannel = (client as AVClient).registrationChannel;
     expect(registrationChannel).to.be.a('string');
     expect(registrationChannel).to.not.be.undefined;
-
-    // Verify it's a valid JWT with three parts
-    const parts = registrationChannel.split('.');
-    expect(parts).to.have.lengthOf(3);
-
-    // Decode and verify the payload contains the expected claims
-    const decoded = jwtDecode(registrationChannel);
-    expect(decoded.sub).to.equal('channel');
-    expect(decoded.iat).to.be.a('number');
-  });
-
-  it('generates a JWT with ES256 algorithm', async () => {
-    const privateKeyHex = '0b4cef5f4e3e1f0a9d8c7b6a594837261504f3e2d1c0b9a8f7e6d5c4b3a29100';
-
-    await client.setRegistrationChannel(privateKeyHex);
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const registrationChannel = (client as any).registrationChannel;
-    // Decode the header
-    const header = JSON.parse(Buffer.from(registrationChannel.split('.')[0], 'base64url').toString());
-    expect(header.alg).to.equal('ES256');
-  });
-
-  it('produces different JWTs for different private keys', async () => {
-    const privateKey1 = '0b4cef5f4e3e1f0a9d8c7b6a594837261504f3e2d1c0b9a8f7e6d5c4b3a29100';
-    const privateKey2 = '1c5def6f5f4e3e1f0a9d8c7b6a594837261504f3e2d1c0b9a8f7e6d5c4b3a292';
-
-    await client.setRegistrationChannel(privateKey1);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const channel1 = (client as any).registrationChannel;
-
-    await client.setRegistrationChannel(privateKey2);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const channel2 = (client as any).registrationChannel;
-
-    expect(channel1).to.not.equal(channel2);
-  });
-
-  it('clears a previously set channel when called with undefined', async () => {
-    const privateKeyHex = '0b4cef5f4e3e1f0a9d8c7b6a594837261504f3e2d1c0b9a8f7e6d5c4b3a29100';
-
-    await client.setRegistrationChannel(privateKeyHex);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect((client as any).registrationChannel).to.be.a('string');
-
-    await client.setRegistrationChannel(undefined);
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect((client as any).registrationChannel).to.be.undefined;
   });
 });
