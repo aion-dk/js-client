21f1f56935984d9609c8199954291b7f5ac38884
ebadia <enric.badia@gmail.com>
2026-06-18 08:33:50 +0200

    Optimize ByteArrayReader string padding removal by replacing regex with manual null byte trimming

--- lib/av_client/encoding/byte_array_reader.ts
+++ lib/av_client/encoding/byte_array_reader.ts
@@ -1,23 +1,20 @@
-const stringPadding = /\0+$/
-
 export class ByteArrayReader {
-  private pointer: number
-  private byteArray: Uint8Array
+  private pointer: number = 0
+  private readonly byteArray: Uint8Array
 
   /**
    * constructs a ByteArrayReader as a wrapper around the given byte array
-   * 
+   *
    * @param byteArray the wrapped byte array
    */
   constructor(byteArray: Uint8Array){
-    this.pointer = 0
     this.byteArray = byteArray
   }
 
   /**
    * Reads an amount of bytes from the position of the pointer.
    * The requested bytes will be returned as a Uint8Array.
-   * 
+   *
    * @throws Error('Out of bounds') in there are not enough bytes left to read
    * @param length amount of bytes to read
    * @returns array of bytes
@@ -35,7 +32,7 @@ export class ByteArrayReader {
   /**
    * Reads the remainder of the array to check if we have any more bytes
    * that are not null.
-   * 
+   *
    * @returns true if there are more bytes to read, false otherwise
    */
   hasMore(): boolean {
@@ -45,7 +42,7 @@ export class ByteArrayReader {
 
   /**
    * Reads an amount of bytes and converts to a positive integer
-   * 
+   *
    * @param size amount of bytes to read and interpret to number
    * @returns number
    */
@@ -56,15 +53,18 @@ export class ByteArrayReader {
 
   /**
    * Reads an amount of bytes and converts to a (utf8) string.
-   * Any zero bytes by the end of the string is considered padding 
+   * Any zero bytes by the end of the string is considered padding
    * and are discarded before the string is returned.
-   * 
+   *
    * @param size amount of bytes to read and interpret as string
    * @returns string
    */
   readString(size: number): string {
     const bytes = this.read(size)
-    const text = new TextDecoder().decode(bytes)
-    return text.replace(stringPadding, '')
+    let end = bytes.length
+    while (end > 0 && bytes[end - 1] === 0) {
+      end--
+    }
+    return new TextDecoder().decode(bytes.subarray(0, end))
   }
 }
